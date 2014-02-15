858a46df67e79443533eaa32deb6fb4f6ad0efff
biradar.m2@gmail.com <biradar.m2@gmail.com@a7697e6b-7b47-7ecc-f82c-3215ba9f1654>
2014-02-15 08:06:48 +0000

    This Project is used to create features project for servicemix .

---
+++ trunk/webservice-feature/src/main/java/com/taggy/feature/webservice/feature/App.java
@@ -0,0 +1,13 @@
+package com.taggy.feature.webservice.feature;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
